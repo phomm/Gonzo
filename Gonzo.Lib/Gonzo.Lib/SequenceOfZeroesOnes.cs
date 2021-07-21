5ed6c1b10023f6f2cb25323a9d90815fdb97124f
phomm <phomm@mail.ru>
2021-07-22 02:23:36 +0500

    Added basic class library for task logic

---
+++ Gonzo.Lib/Gonzo.Lib/SequenceOfZeroesOnes.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Gonzo.Lib
+{
+    public class SequenceOfZeroesOnes
+    {
+        private readonly IEnumerable<int> _sequence;
+
+        public SequenceOfZeroesOnes(string rawSequence)
+            : this(rawSequence.Split(',').Select(s => s.Trim()).Where(s => s == "0" || s == "1").Select(int.Parse)) { }
+
+        public SequenceOfZeroesOnes(IEnumerable<int> rawSequence)
+        {
+            var sequence = rawSequence.Where(x => x == 0 || x == 1);
+            if (!sequence.Any())
+                throw new ArgumentException("Initial sequence contains no 0 or 1");
+            _sequence = sequence;
+        }
+
+        public int CalculateMaxOnesAfterRemoveItem()
+        {
+            return _sequence.Count();
+        }
+    }
+}
