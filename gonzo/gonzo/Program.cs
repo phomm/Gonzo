4007bcb3762595423e2feab0ca9a7b24e1cfabe3
phomm <phomm@mail.ru>
2021-07-22 02:24:11 +0500

    Implemented a program for user testing

--- gonzo/gonzo/Program.cs
+++ gonzo/gonzo/Program.cs
@@ -2,11 +2,31 @@
 
 namespace Gonzo
 {
+    using Gonzo.Lib;
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var again = true;
+            while (again)
+            {
+                Console.Clear();
+                Console.WriteLine("Please, input a sequence of 0 and 1 splitted by commas");
+                var rawSequence = Console.ReadLine();
+                try
+                {
+                    var sequence = new SequenceOfZeroesOnes(rawSequence);
+                    var result = sequence.CalculateMaxOnesAfterRemoveItem();
+                    Console.WriteLine($"Max Ones After Remove Any Item: {result}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+                Console.WriteLine("Again? (y/n): ");
+                again = Console.ReadLine() == "y";
+            }            
         }
     }
 }
