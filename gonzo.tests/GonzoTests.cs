a06ca71762312c688c35becb0604e8bb22ca08a6
phomm <phomm@mail.ru>
2021-07-22 02:25:20 +0500

    Incorporated library class into tests

--- gonzo.tests/GonzoTests.cs
+++ gonzo.tests/GonzoTests.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 
 namespace Gonzo.Tests
 {
+    using Gonzo.Lib;
     public class GonzoTests
     {
         [TestCase(new[] { 0, 0 }, 0)]
@@ -12,9 +13,10 @@ namespace Gonzo.Tests
         [TestCase(new[] { 1, 1, 0, 1, 1 }, 4)]
         [TestCase(new[] { 1, 1, 0, 1, 1, 0, 1, 1, 1 }, 5)]
         [TestCase(new[] { 1, 1, 0, 1, 1, 0, 1, 1, 1, 0 }, 5)]
-        public void Test_maxOnesAfterRemoveItem_withProvidedTestCases(IEnumerable<int> sequence, int expected)
+        public void Test_maxOnesAfterRemoveItem_withProvidedTestCases(IEnumerable<int> candidate, int expected)
         {
-            var actual = -1;
+            var sequence = new SequenceOfZeroesOnes(candidate);
+            var actual = sequence.CalculateMaxOnesAfterRemoveItem();
             Assert.AreEqual(expected, actual);
         }
     }
